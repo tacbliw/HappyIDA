9396c6ae5f04b69823cc42f2a42c9fbcd7d829dd
terrynini38514 <terrynini38514@gmail.com>
2025-06-11 16:28:35 +0800

    feat: add double-clicking to navigate to the member function

--- ida_happy/__init__.py
+++ ida_happy/__init__.py
@@ -43,6 +43,21 @@ def tag_text(text, tag):
     FMT = '%c%c%' + '0%dX' % ida_lines.COLOR_ADDR_SIZE + '%s'
     return FMT % (ida_lines.COLOR_ON, ida_lines.COLOR_ADDR, tag, text)
 
+class FuncChooser(idaapi.Choose):
+    def __init__(self, title, cols, items):
+        super(FuncChooser, self).__init__(title, cols, flags=idaapi.Choose.CH_MODAL)
+        self.items = items
+        self.icon  = 5
+
+    def OnGetSize(self):
+        return len(self.items)
+
+    def OnGetLine(self, n):
+        return self.items[n]
+
+    def OnSelectLine(self, n):
+        return True
+    
 class HexraysFuncLabelHook(ida_hexrays.Hexrays_Hooks):
     def __init__(self):
         super().__init__()
@@ -50,7 +65,7 @@ class HexraysFuncLabelHook(ida_hexrays.Hexrays_Hooks):
         # decompile view won't handle 'N' keyboard event, so we have to hook it here
         class UIFuncArgsHooks(ida_kernwin.UI_Hooks):
             def preprocess_action(self, action):
-                if HexraysFuncLabelHooks.label_actions(action):
+                if HexraysFuncLabelHook.label_actions(action):
                     return 1
 
                 return 0
@@ -163,6 +178,9 @@ class HexraysDoubleClickHook(ida_hexrays.Hexrays_Hooks):
         if self.double_click_to_retype(vdui):
             return 1
 
+        if self.double_click_to_navigate(vdui):
+            return 1
+        
         return 0
 
     @undoable
@@ -344,6 +362,44 @@ class HexraysDoubleClickHook(ida_hexrays.Hexrays_Hooks):
 
             return HandleStatus.HANDLED
 
+    def double_click_to_navigate(self, vdui) -> HandleStatus:
+        
+        if not vdui.get_current_item(ida_hexrays.USE_MOUSE) or not vdui.in_ctree():
+            return HandleStatus.NOT_HANDLED
+
+        if vdui.item.citype == idaapi.VDI_EXPR and vdui.item.e.is_expr():
+            expr = idaapi.tag_remove(vdui.item.e.print1(None))
+            if "->" in expr:
+                name = expr.split("->")[-1].strip()
+                addr = idc.get_name_ea_simple(name)
+                if addr != idaapi.BADADDR:
+                    idc.jumpto(addr)
+                    return HandleStatus.HANDLED
+                else:
+                    func_names = [ idc.get_func_name(f) for f in idautils.Functions() ]
+                    matches = []
+                    #TODO: user can limit output size
+                    #TODO: make this fuzzy search, like std::vector will also match std::__1::vector
+                    for func_name in func_names:
+                        dename = idc.demangle_name(func_name, idc.get_inf_attr(idc.INF_LONG_DN))
+                        if name in func_name or (dename is not None and name in dename):
+                            matches.append((dename, func_name))
+                    if matches:
+                        items = [(dename, "%08X" % idc.get_name_ea_simple(func_name)) for (dename,func_name) in matches]
+                        cols = [
+                            ["Name",    40 | idaapi.Choose.CHCOL_PLAIN],
+                            ["Address", 16 | idaapi.Choose.CHCOL_HEX],
+                        ]
+                        chooser = FuncChooser("Function matches for '%s'" % name, cols, items)
+                        idx = chooser.Show(True)
+                        if idx not in (idaapi.Choose.NO_SELECTION, idaapi.Choose.EMPTY_CHOOSER):
+                            sel_name, sel_addr_str = items[idx]
+                            idc.jumpto(int(sel_addr_str, 16))
+                            info("Jump to '%s'" % sel_name)
+                            return HandleStatus.HANDLED
+                    else:
+                        info("No close matches for '%s'" % name)
+                    
         return HandleStatus.NOT_HANDLED
 
     def retype_pseudocode_var(self, func_ea, varname, tinfo):
